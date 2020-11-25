2642dd0950f80e6930a3e4e114fe2cc77d4cf193
Austine Gomez <austine.gomez@yahoo.com>
2020-11-24 22:55:09 -0600

    css

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import SpacingGrid from '../Components/Grid';
+import image2 from "../Components/Navbar/red.png";
+
+
 const About = () => {
     return(
         <div>
-        <h1 className="about-h">About</h1>
-            <SpacingGrid />
+        <div className="aboutlogo">
+        <img src={image2} style={{height:200}}/>
+        <h1 style={{fontSize:120}}>CodeSource</h1>
         </div>
+        <h1 className="about-h">About</h1>
+        <SpacingGrid />
+      </div>
       );
     };
     
