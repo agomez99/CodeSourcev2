c1c8544103c174ed68666873b59992d2670e5629
Austine Gomez <austine.gomez@yahoo.com>
2021-06-15 23:24:35 -0500

    button not functional

--- client/src/Components/NewBlog/NewBlog.js
+++ client/src/Components/NewBlog/NewBlog.js
@@ -5,8 +5,25 @@ import Container from "@material-ui/core/Container";
 //import TextField from '@material-ui/core/TextField';
 //import SearchIcon from "@material-ui/icons/Search";
 //import InputAdornment from "@material-ui/core/InputAdornment";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 import "./styles.css";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 const Blog = () => {
   const [loading, setLoading] = useState(false);
@@ -46,12 +63,52 @@ const Blog = () => {
     return getPosts();
   };
 
-  
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div>
-      <Container >
-        <div>
-          <h1 className="blog-header">Blog Posts</h1>
+      <Container>
+      <h1 className="blog-header">Blog Posts</h1>
+
+        <div >
+          <div style={{display:"block", float:"center", marginLeft:"100px"}}>
+            <Button className={classes.button} onClick={handleOpen}>
+              Sort my blogs
+            </Button>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-controlled-open-select-label">
+                Blogs
+              </InputLabel>
+              <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={open}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                value={age}
+                onChange={handleChange}
+              >
+                <MenuItem value=""></MenuItem>
+                <MenuItem value={10}>Name</MenuItem>
+                <MenuItem value={20}>Date</MenuItem>
+                <MenuItem value={30}>Technologies</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
           {getContent()}
         </div>
       </Container>
